4367892375f476922199276b00e65cb9882553c3
akashsingh <aakashsingh2702@gmail.com>
2022-12-09 22:42:12 +0530

    Inotial C0ommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
 
-function App() {
+export default function App() {
+  const urls = [
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  ];
+  const [currentUrlIndex, setCurrentUrlIndex] = useState(0);
+  const [showNextButton, setShowNextButton] = useState(false);
+  const videoRef = useRef();
+
+  // console.log(videoRef.current?.currentTime);
+  // // setCurrentTime(videoRef.current?.currentTime);
+  // const onSeek = (seek) => {
+  //   //Handler for change in seekbar
+  //   videoRef.current.seek(seek);
+  // };
+
+  // useEffect(() => {
+  //   const vid = document.getElementById("vid");
+  //   vid.addEventListener("seeking", function () {
+  //     alert("Seek operation began!");
+  //   });
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <video
+        id="vid"
+        style={{ maxWidth: "100%", width: "800px", margin: "0 auto" }}
+        playsInline
+        muted
+        controls
+        onEnded={() => setShowNextButton(true)}
+        src={urls[currentUrlIndex]}
+        ref={videoRef}
+      />
+      {showNextButton && (
+        <button
+          onClick={() => {
+            setCurrentUrlIndex(
+              (prevUrlIndex) => (prevUrlIndex + 1) % urls.length
+            );
+            setShowNextButton(false);
+          }}
+          style={{
+            position: "absolute",
+            zIndex: 10,
+            fontSize: "2em",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          next
+        </button>
+      )}
     </div>
   );
 }
-
-export default App;
